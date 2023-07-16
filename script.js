5e878c716ec030b98a819e15fcf36557215c9905
SamNijin <samnijin562@gmail.com>
2023-07-16 15:34:45 +0530

    Responsive design

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ window.addEventListener("scroll", function () {
 
   prevScrollPos = currentScrollPos;
 });
+
+// Mobile Navbar
+var mobile_prevScrollPos = window.pageYOffset;
+
+var mobile_navbar = document.getElementById("mobile-navbar");
+
+window.addEventListener("scroll", function () {
+  var currentScrollPos = window.pageYOffset;
+
+  if (mobile_prevScrollPos > currentScrollPos) {
+    mobile_navbar.style.opacity = "1";
+    mobile_navbar.classList.add("sticky");
+    mobile_navbar.style.filter = "blur(0)";
+  } else {
+    mobile_navbar.style.opacity = "0";
+    mobile_navbar.style.filter = "blur(1px)";
+    mobile_navbar.classList.remove("sticky");
+  }
+
+  mobile_prevScrollPos = currentScrollPos;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mobileMenu = document.querySelector(".mobile-menu");
+  var mobileMenuOverlay = document.querySelector(".mobile-menu-overlay");
+
+  mobileMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+    mobileMenuOverlay.classList.toggle("active");
+    mobile_navbar.style.filter = "";
+
+    if (mobileMenu.classList.contains("active")) {
+      // Disable scroll
+      document.body.style.overflow = "hidden";
+    } else {
+      // Enable scroll
+      document.body.style.overflow = "auto";
+    }
+  });
+
+  var closeButton = document.querySelector(".close-button");
+  closeButton.addEventListener("click", function () {
+    mobileMenu.classList.remove("active");
+    mobileMenuOverlay.classList.remove("active");
+    document.body.style.overflow = "auto"; // Enable scroll
+  });
+});
