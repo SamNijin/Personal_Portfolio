03fe4ec4931eb2f24451b287b41f18453c149745
SamNijin <samnijin562@gmail.com>
2023-07-16 12:16:30 +0530

    updated nav behavior

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var prevScrollPos = window.pageYOffset;
-var navbar = document.getElementById('navbar');
+var navbar = document.getElementById("navbar");
 
-window.addEventListener('scroll', function () {
-    var currentScrollPos = window.pageYOffset;
+window.addEventListener("scroll", function () {
+  var currentScrollPos = window.pageYOffset;
 
-    if (prevScrollPos > currentScrollPos) {
-        navbar.style.opacity = '1';
-        navbar.classList.add('sticky');
-    } else {
-        navbar.style.opacity = '0';
-        navbar.classList.remove('sticky');
-    }
+  if (prevScrollPos > currentScrollPos) {
+    navbar.style.opacity = "1";
+    navbar.classList.add("sticky");
+    navbar.style.filter = "blur(0)";
+  } else {
+    navbar.style.opacity = "0";
+    navbar.style.filter = "blur(1px)";
+    navbar.classList.remove("sticky");
+  }
 
-    prevScrollPos = currentScrollPos;
+  prevScrollPos = currentScrollPos;
 });
